7861a746fa70c2c2ffe5801db7a476db26eefb50
leandroTF <leandrotrindade.lemobs@gmail.com>
2020-11-28 18:16:45 -0300

    Revert "Adicionando validation pipes"
    This reverts commit b6cb84a8b3a7da77b5dc1d8a8c6a942033a767d9.

--- src/image/dto/image.dto.ts
+++ src/image/dto/image.dto.ts
@@ -1,9 +1,4 @@
-import { IsInt, IsString } from "class-validator"
-
 export class ImageDto {
-    @IsInt()
     userId: number
-
-    @IsString()
     title: string
 }
--- src/user/dto/user.dto.ts
+++ src/user/dto/user.dto.ts
@@ -1,9 +1,5 @@
-import { IsEmail, IsString } from 'class-validator';
-
 export class UserDto {
-    @IsString()
     name: string
-
-    @IsEmail()
     email: string
+    photoUrl: string 
 }
